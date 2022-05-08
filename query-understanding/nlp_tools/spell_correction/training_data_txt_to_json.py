72d7e8b734c9fe45dbd915974c65751531758ff4
erwaen <erikwasmosy98@hotmail.com>
2022-05-08 18:47:57 -0400

    🔥 remove or moved some files

--- query-understanding/nlp_tools/spell_correction/training_data_txt_to_json.py
+++ query-understanding/nlp_tools/spell_correction/training_data_txt_to_json.py
@@ -2,7 +2,7 @@ import json
 def word_frecuency_of_crea_total():
     print("obteniendo datos....")
     word_frequency = {} # {'hola': 1232332, 'el': 1203033}
-    with open('CREA_total.txt', 'r', encoding='latin-1' ) as spanish_file:
+    with open('./dataset/spanish_words_frequency.txt', 'r', encoding='latin-1' ) as spanish_file:
         all_lines = spanish_file.read()
         separate_in_lines = all_lines.split('\n')[1:]
     
@@ -18,7 +18,7 @@ def word_frecuency_of_crea_total():
             word_frequency[word] = frecuency_of_the_word
 
 
-    with open("training_data_spanish.json", "w") as outfile:
+    with open("./dataset/training_data_spanish.json", "w") as outfile:
         json.dump (word_frequency, outfile, indent=4)
 
         
