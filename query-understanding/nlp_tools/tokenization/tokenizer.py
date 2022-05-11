41c85645c0ed268ea54776539645f0ad657cfd8c
erwaen <erikwasmosy98@hotmail.com>
2022-05-11 13:51:26 -0400

    🚧 working in tokenizer method

--- query-understanding/nlp_tools/tokenization/tokenizer.py
+++ query-understanding/nlp_tools/tokenization/tokenizer.py
@@ -1,7 +1,24 @@
 import spacy
+import pickle
+
+
 
 def tokenizer(sentence: str)-> list:
-    pass
+    """Give a sentence 
+    this function separate the sentence in units called 'tokens'
+
+    where a token can be a word or some '.', '!', '?' ',', etc..
+
+    e.g:
+     tokenizer('hola que tal!.')
+     -> ['hola', 'que', 'tal', '!', '.']
+     wehere in the list the 'hola' is a token and 'que' is another token
+    """
+    nlp = spacy.load('es_core_news_sm')
+    doc = nlp(sentence)
+    print(doc.text)
+    print([(w.text, w.pos_) for w in doc])
 
 
-    
+if __name__ == '__main__':
+    tokenizer('haciendo que tal amigos... ')
