fdcf0418025351412d319e60f5dc19a6d657ff8b
erwaen <erikwasmosy98@hotmail.com>
2022-05-12 14:39:38 -0400

    adding abastracts functions

--- query-understanding/text_normalization.py
+++ query-understanding/text_normalization.py
@@ -1,26 +1,33 @@
-from tokenize import String
+import unidecode
 from nlp_tools.spell_correction import spell_correction
+from nlp_tools.tokenization import tokenizer
 
 def text_normalization(sentence: str):
-    #sentence = normalize_accents(sentence: str) -> str
 
+    # put here a function to remove . , ? ! ect...
+    
     words_corrected = [spell_correction.correction(word) for word in sentence.split()]
     corrected_sentence = ' '.join(words_corrected)
-    print(corrected_sentence)
+
+    corrected_sentence = normalize_accents(corrected_sentence)
+
+    tokenized = tokenizer.tokenizer(corrected_sentence)
+
+    return tokenized
+    
+
     
 
 
 
 def normalize_accents(sentece: str) -> str:
     """Change whatever word with an accent to the ASCII version of the letter
-
-
     e.g: 
-        'áÁ' ->
+        'áÁ' ->'aA'
              
     """
-    pass
+    return unidecode.unidecode(sentece)
 
 
 if __name__ == "__main__":
-    print(text_normalization("hola que tall vamoss al cune hoy?"))
+    print(text_normalization("hola que . , tall vámoss al cune hoy?"))
