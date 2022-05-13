8a4f1bd1f66013a548c7c12d9996d244c9b79af6
giodueck <giodueck@gmail.com>
2022-05-13 14:16:39 -0400

    Add index_server tests

--- index/gpp_index.py
+++ index/gpp_index.py
@@ -1,4 +1,12 @@
+import hashlib
 import pickle
+import string
+from typing import Counter
+
+import nltk
+import unidecode
+from bs4 import BeautifulSoup
+
 
 def page_count(page):
     '''Auxiliary function for sorting pages in the index
@@ -12,37 +20,42 @@ class GPP_Index:
     The dump() method will create a pickle file with the dictionary, the index itself, which will be used
     for searching and permanent storage of the index.'''
     
-    def __init__(self, pickled_file: str = None):
-        if pickled_file is None:
+    def __init__(self, pickled_index: str = None, pickled_urls: str = None):
+        if pickled_index is None:
             self.index = dict()
+            self.indexed_urls = dict() # format = {'https://example.com': 'hash'}
         else:
-            with open(pickled_file, 'rb') as file:
+            with open(pickled_index, 'rb') as file:
                 self.index = pickle.load(file)
+                
+            # List of urls indexed makes it possible to tell if updating or indexing for the first time
+            if pickled_urls is not None:
+                with open(pickled_urls, 'rb') as file:
+                    self.indexed_urls = pickle.load(file)
+            else: # building this list cannot be done without the hashes of the scraped content
+                raise "List of urls and it's content hashes not present, cannot load the index"                
     
     def add_entry(self, word: str, url: str, count: int):
-        '''Adds or updates a word's entry with the new url and count.
+        '''Adds a word's entry with the url of and appearance count in a page.'''
         
-        If the count is <= 0, the entry is removed.'''
-        if word in self.index:
+        if count <= 0:
+            return
+        
+        # Add new word
+        if word not in self.index:
+            self.index[word] = [{'url': url, 'count': count}]
+        # Or add to existing word
+        else:
+            # Check if the word has not been indexed on that page before (for updates)
             if len([page for page in self.index[word] if page['url'] == url]) == 0:
-                if count > 0:
-                    self.index[word].append({'url': url, 'count': count})
-                else:
-                    for page in self.index[word]:
-                        if page['url'] == url:
-                            self.index[word].remove(page)
-                            break
+                self.index[word].append({'url': url, 'count': count})
             else:
+                # If it has, just update the count. Shouldn't ever happen but just in case
                 for page in self.index[word]:
                     if page['url'] == url:
-                        if count > 0:
-                            page['count'] = count
-                        else:
-                            self.index[word].remove(page)
+                        page['count'] = count
                         break
-        elif count > 0:
-            self.index[word] = [{'url': url, 'count': count}]
-    
+        
     def del_entry(self, word: str, url: str):
         '''Removes an entry for a word using the url. Calls add_entry with count=0'''
         self.add_entry(word, url, 0)
@@ -51,13 +64,15 @@ class GPP_Index:
         '''Removes a word from the index, deleting all page entries for it.'''
         self.index.pop(word)
     
-    def dump(self, filename: str = 'index.pickle'):
+    def dump(self, index_filename: str = 'index.pickle', urls_filename: str = 'urls.pickle'):
         '''Dumps self.index into a pickled file. Sorts before dumping.
         
         Unpickling will result in a dict, not an Index object.'''
         self.sort()
-        with open(filename, 'wb') as file:
+        with open(index_filename, 'wb') as file:
             pickle.dump(self.index, file)
+        with open(urls_filename, 'wb') as file:
+            pickle.dump(self.indexed_urls, file)
 
     def sort(self):
         '''Sort pages associated with words by count, and words themselves alphabetically'''
@@ -65,18 +80,63 @@ class GPP_Index:
             page.sort(key=page_count, reverse=True)
         # convert dict to list of (token, pages) tuples, sort (this will use token), and convert back to dict before returning
         self.index = dict(sorted(list(self.index.items())))
+    
+    def build_index(self, html_content: str, url: str):
+        '''# WIP
+        
+        Goes through the return data of a scraped page, indexing the text.
+        
+        Assumed format: {'content': ["<tags>text</tags>"], 'links': ["link1", "link2", ...]}'''
+        
+        # TBD:
+        # - Where are the scraped pages being stored?
+        # - How do I find the url to a page? Currently there is page content and links only
+        # - Do I bother with the Markov chain idea?
+        
+        # Check if the page has been indexed before, if so this is a job for update_index()
+        content_hash = hashlib.md5(html_content.encode())
+        if url in self.indexed_urls:
+            if self.indexed_urls[url] != content_hash.hexdigest():
+                self.update_index(html_content, url)
+                self.indexed_urls[url] = content_hash.hexdigest()
+            return
+        # Add to list of urls
+        self.indexed_urls[url] = content_hash.hexdigest()
+        
+        # Start by adding in BeautifulSoup for html parsing
+        content = BeautifulSoup(html_content, 'html.parser')
+        
+        page_text = content.text.strip()
+        # normalize accents and remove weird symbols
+        page_text = page_text.translate('', '', page_text.maketrans('°')) # add more if necessary
+        page_text = unidecode.unidecode(page_text)
+        # remove punctuation
+        # no_punct_page_text = page_text.translate(page_text.maketrans('', '', string.punctuation))
 
+        # Tokenize
+        tokens = nltk.word_tokenize(page_text)
+        # no_punct_tokens = nltk.word_tokenize(no_punct_page_text)
+        
+        # Count tokens
+        counts = dict(Counter(tokens))
+        # no_punct_counts = dict(Counter(no_punct_tokens))
+        
+        # Index
+        for token, count in counts.items():
+            # Check that avoids tokens like ',' being indexed
+            if len([symbol for symbol in token if symbol in string.punctuation]) > 0:
+                self.add_entry(token, url, count)
+        # No punctuation version, is that needed? NLTK separates most punctuation into its own tokens anyways, easy to filter
+
+    def update_index(self, html_content: str, url: str):
+        '''Handles updating a page's indexes, particularly deleting words that were removed/replaced.'''
+
+        # TODO
+        # Remove previous entries entirely
+        # Read entries
+        pass
+    
 if __name__ == '__main__':
     # Examples
-    idx = GPP_Index()
-    idx.add_entry('test', 'www.test.org', 5)
-    idx.add_entry('test', 'www.test.com', 12)
-    idx.add_entry('tester', 'www.test.com', 1)
-    idx.add_entry('test', 'www.test.com', 21)
-    idx.add_entry('tests', 'www.test.com', 13)
-    print(idx.index)
-    idx.del_entry('tester', 'www.test.com')
-    print(idx.index)
-    idx.del_word('tester')
-    print(idx.index)
-    idx.dump()
+    pass
+    # index creation example, too tired rn
