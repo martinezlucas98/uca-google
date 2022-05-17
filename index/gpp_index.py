398b5c227a0b9dab2ddd07e7a0f12fe3e7c63be0
giodueck <giodueck@gmail.com>
2022-05-17 12:40:09 -0400

    Develop index building

--- index/gpp_index.py
+++ index/gpp_index.py
@@ -29,32 +29,36 @@ class GPP_Index:
         
         # Load from pickled file
         if pickled_gpp_index is not None:
-            with open(pickled_gpp_index, 'rb') as file:
-                instance = pickle.load(file)
-            for attr, value in instance.__dict__.items():
-                self.__dict__[attr] = value
+            try:
+                with open(pickled_gpp_index, 'rb') as file:
+                    instance = pickle.load(file)
+                for attr, value in instance.__dict__.items():
+                    self.__dict__[attr] = value
+            except FileNotFoundError:
+                pass # Only try to load if the file exists
     
-    def add_entry(self, word: str, url: str, count: int, date: datetime, links: list, title: str, description: str):
-        '''Adds a word's entry with the url of and appearance count in a page.'''
+    def add_entry(self, token: str, url: str, count: int, date: datetime, links: list, title: str, description: str):
+        '''Adds a word's (token) entry with the url of and appearance count in a page.'''
         
         if count <= 0:
             return
         
-        # Add new word
-        if word not in self.index:
-            self.index[word] = [{'url': url, 'count': count, 'date': date, 'title': title, 'description': description}]
-        # Or add to existing word
+        entry = {'url': url, 'count': count, 'date': date, 'title': title, 'description': description, 'links': links}
+        # Add new token
+        if token not in self.index:
+            self.index[token] = [entry]
+        # Or add to existing token
         else:
             # Updates will involve deleting entries, so repeated entries should not happen
-            self.index[word].append({'url': url, 'count': count, 'date': date, 'title': title, 'description': description})
+            self.index[token].append(entry)
         
     def del_page(self, url: str):
-        '''Removes all entries associated with a url.'''
+        '''Removes all entries associated with a url. May leave empty tokens on the index, call strip() to remove them'''
         for token, page in [(token, page) for token in self.index for page in self.index[token] if page['url'] == url]:
             self.index[token].remove(page)
     
     def dump(self, filename: str = 'gpp_index.pickle'):
-        '''Dumps the obj so it can be updated later.'''
+        '''Dumps self so it can be updated later.'''
         self.strip()
         self.sort()
         with open(filename, 'wb') as file:
@@ -93,65 +97,59 @@ class GPP_Index:
         # - How do I find the url to a page? Currently there is page content and links only
         # - Do I bother with the Markov chain idea?
         
-        # Check if the page has been indexed before, if so this is a job for update_index()
+        # Check if the page has been indexed before, if so delete the page off the index as if it was never indexed
         content_hash = hashlib.md5(html_content.encode())
-        is_update = False
         if url in self.indexed_urls:
             if self.indexed_urls[url] != content_hash.hexdigest():
-                is_update = True
+                self.del_page(url)
             else:
                 return # as this page is up to date on the index
         
         # Start by adding in BeautifulSoup for html parsing
-        content = BeautifulSoup(html_content, 'html.parser')
+        soup = BeautifulSoup(html_content, 'html.parser')
         
-        page_text = content.text.strip()
-        # normalize accents and remove weird symbols
-        page_text = page_text.translate('', '', page_text.maketrans('°')) # add more if necessary
-        page_text = unidecode.unidecode(page_text)
-        # remove punctuation
-        # no_punct_page_text = page_text.translate(page_text.maketrans('', '', string.punctuation))
+        # Get title and description, both may be None
+        title = soup.find('title')
+        if soup.title is not None:
+            title = soup.title.text.strip()
 
-        # Tokenize
-        tokens = nltk.word_tokenize(page_text)
-        # no_punct_tokens = nltk.word_tokenize(no_punct_page_text)
+        description = soup.find('meta', {'name':'description'})
+        if description is None:
+            description = soup.find('meta', {'name':'Description'})
+        if description is not None:
+            description = description['content']
         
-        # Count tokens
-        counts = dict(Counter(tokens))
-        # no_punct_counts = dict(Counter(no_punct_tokens))
         
-        # Index
-        if is_update:
-            self.update_index(html_content, url, date, links)
-            return
-        
-        for token, count in counts.items():
-            # Check that avoids tokens like ',' being indexed
-            if len([symbol for symbol in token if symbol not in string.punctuation]) > 0:
-                self.add_entry(token, url, count, date, links)
-        # No punctuation version, is that needed? NLTK separates most punctuation into its own tokens anyways, easy to filter
+        # No body, no words to index. The head alone is not interesting enough
+        if soup.body is not None:
+            body_text = soup.body.get_text(' ').strip()
+            # normalize accents and remove weird symbols
+            body_text = body_text.translate(body_text.maketrans('', '', '°')) # add more if necessary
+            body_text = unidecode.unidecode(body_text)
+
+            # Tokenize
+            tokens = [token.lower() for token in nltk.word_tokenize(body_text)]
+            
+            # Count tokens
+            counts = dict(Counter(tokens))
+            
+            # Index
+            for token, count in counts.items():
+                # Check that avoids tokens like ',' being indexed
+                if len([symbol for symbol in token if symbol not in string.punctuation]) > 0:
+                    self.add_entry(token, url, count, date, [link for link in links if link.startswith('http')], title, description)
+            # No punctuation version, is that needed? NLTK separates most punctuation into its own tokens anyways, easy to filter
 
         # Done, add to list of urls
-        self.indexed_urls[url] = content_hash.hexdigest()
-
-    def update_index(self, html_content: str, url: str, date: datetime, links: list):
-        '''Handles updating a page's indexes, particularly deleting words that were removed/replaced.'''
-
-        # TODO
-        # Remove previous entries entirely
-        # Read entries
-        pass
-    
-        # Done, add to list of urls
-        content_hash = hashlib.md5(html_content.encode())
         self.indexed_urls[url] = content_hash.hexdigest()
     
 if __name__ == '__main__':
     # Examples
-    test_index = GPP_Index()
-    test_index.add_entry('word', 'example.com', 15, datetime(2022, 5, 17), [], 'Title', 'Description text')
-    test_index.add_entry('example', 'example.com', 11, datetime(2022, 5, 17), [], 'Title', 'Description text')
-    test_index.add_entry('word', 'example.org', 17, datetime(2022, 5, 17), [], 'Title 2', 'Description text 2')
-    print(test_index.index)
-    test_index.del_page('example.com')
-    print('\n', test_index.index)
+    test_index = GPP_Index('test_index.pickle')
+    print(len(test_index.index))
+    import json
+    with open('outfile.json') as file:
+        page = json.load(file)
+    test_index.build_index(page['content'][0], 'https://universidadcatolica.edu.py', datetime.now(), page['links'])
+    print(len(test_index.index))
+    test_index.dump('test_index.pickle')
