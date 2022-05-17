a7cc04a3bb4e697f64233fb63a11e0f56f7aaa3e
giodueck <giodueck@gmail.com>
2022-05-17 11:38:40 -0400

    Change add_entry; mv del_word, _entry to del_page

--- index/gpp_index.py
+++ index/gpp_index.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import hashlib
 import pickle
 import string
@@ -33,7 +34,7 @@ class GPP_Index:
             for attr, value in instance.__dict__.items():
                 self.__dict__[attr] = value
     
-    def add_entry(self, word: str, url: str, count: int):
+    def add_entry(self, word: str, url: str, count: int, date: datetime, links: list, title: str, description: str):
         '''Adds a word's entry with the url of and appearance count in a page.'''
         
         if count <= 0:
@@ -41,29 +42,20 @@ class GPP_Index:
         
         # Add new word
         if word not in self.index:
-            self.index[word] = [{'url': url, 'count': count}]
+            self.index[word] = [{'url': url, 'count': count, 'date': date, 'title': title, 'description': description}]
         # Or add to existing word
         else:
-            # Check if the word has not been indexed on that page before (for updates)
-            if len([page for page in self.index[word] if page['url'] == url]) == 0:
-                self.index[word].append({'url': url, 'count': count})
-            else:
-                # If it has, just update the count. Shouldn't ever happen but just in case
-                for page in self.index[word]:
-                    if page['url'] == url:
-                        page['count'] = count
-                        break
+            # Updates will involve deleting entries, so repeated entries should not happen
+            self.index[word].append({'url': url, 'count': count, 'date': date, 'title': title, 'description': description})
         
-    def del_entry(self, word: str, url: str):
-        '''Removes an entry for a word using the url. Calls add_entry with count=0'''
-        self.add_entry(word, url, 0)
-    
-    def del_word(self, word: str):
-        '''Removes a word from the index, deleting all page entries for it.'''
-        self.index.pop(word)
+    def del_page(self, url: str):
+        '''Removes all entries associated with a url.'''
+        for token, page in [(token, page) for token in self.index for page in self.index[token] if page['url'] == url]:
+            self.index[token].remove(page)
     
     def dump(self, filename: str = 'gpp_index.pickle'):
         '''Dumps the obj so it can be updated later.'''
+        self.strip()
         self.sort()
         with open(filename, 'wb') as file:
             pickle.dump(self, file)
@@ -72,18 +64,24 @@ class GPP_Index:
         '''Dumps self.index into a pickled file. Sorts before dumping.
         
         Unpickling will result in a dict, not a GPP_Index object.'''
+        self.strip()
         self.sort()
         with open(index_filename, 'wb') as file:
             pickle.dump(self.index, file)
 
     def sort(self):
-        '''Sort pages associated with words by count, and words themselves alphabetically'''
+        '''Sort pages associated with words by count, and words themselves alphabetically.'''
         for token, page in self.index.items():
             page.sort(key=page_count, reverse=True)
         # convert dict to list of (token, pages) tuples, sort (this will use token), and convert back to dict before returning
         self.index = dict(sorted(list(self.index.items())))
     
-    def build_index(self, html_content: str, url: str):
+    def strip(self):
+        '''Removes words without entries.'''
+        for token in [token for token in self.index if self.index[token] == []]:
+            self.index.pop(token)
+    
+    def build_index(self, html_content: str, url: str, date: datetime, links: list):
         '''# WIP
         
         Goes through the return data of a scraped page, indexing the text.
@@ -97,13 +95,12 @@ class GPP_Index:
         
         # Check if the page has been indexed before, if so this is a job for update_index()
         content_hash = hashlib.md5(html_content.encode())
+        is_update = False
         if url in self.indexed_urls:
             if self.indexed_urls[url] != content_hash.hexdigest():
-                self.update_index(html_content, url)
-                self.indexed_urls[url] = content_hash.hexdigest()
-            return
-        # Add to list of urls
-        self.indexed_urls[url] = content_hash.hexdigest()
+                is_update = True
+            else:
+                return # as this page is up to date on the index
         
         # Start by adding in BeautifulSoup for html parsing
         content = BeautifulSoup(html_content, 'html.parser')
@@ -124,13 +121,20 @@ class GPP_Index:
         # no_punct_counts = dict(Counter(no_punct_tokens))
         
         # Index
+        if is_update:
+            self.update_index(html_content, url, date, links)
+            return
+        
         for token, count in counts.items():
             # Check that avoids tokens like ',' being indexed
-            if len([symbol for symbol in token if symbol in string.punctuation]) > 0:
-                self.add_entry(token, url, count)
+            if len([symbol for symbol in token if symbol not in string.punctuation]) > 0:
+                self.add_entry(token, url, count, date, links)
         # No punctuation version, is that needed? NLTK separates most punctuation into its own tokens anyways, easy to filter
 
-    def update_index(self, html_content: str, url: str):
+        # Done, add to list of urls
+        self.indexed_urls[url] = content_hash.hexdigest()
+
+    def update_index(self, html_content: str, url: str, date: datetime, links: list):
         '''Handles updating a page's indexes, particularly deleting words that were removed/replaced.'''
 
         # TODO
@@ -138,8 +142,16 @@ class GPP_Index:
         # Read entries
         pass
     
+        # Done, add to list of urls
+        content_hash = hashlib.md5(html_content.encode())
+        self.indexed_urls[url] = content_hash.hexdigest()
+    
 if __name__ == '__main__':
     # Examples
     test_index = GPP_Index()
-    test_index.index['word'] = [{'url':'test.com', 'count':42}]
-    test_index.dump('test_index.pickle')
+    test_index.add_entry('word', 'example.com', 15, datetime(2022, 5, 17), [], 'Title', 'Description text')
+    test_index.add_entry('example', 'example.com', 11, datetime(2022, 5, 17), [], 'Title', 'Description text')
+    test_index.add_entry('word', 'example.org', 17, datetime(2022, 5, 17), [], 'Title 2', 'Description text 2')
+    print(test_index.index)
+    test_index.del_page('example.com')
+    print('\n', test_index.index)
