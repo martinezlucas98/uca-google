bbd300417d1f42b0b610a8139713be97339cc378
bamy16 <60363312+bamy16@users.noreply.github.com>
2022-05-20 01:39:27 -0400

    Update expand_query.py

--- query-understanding/expand_query.py
+++ query-understanding/expand_query.py
@@ -1,7 +1,7 @@
 from text_normalization import  normalize
 from nlp_tools.named_entity import get_entity
 from nlp_tools.part_of_speech import get_part_of_speech
-
+from nlp_tools.query_expansion.query_expansion import query_expansion, entity_recongnition
 
 def expand_query(sentence:str) -> dict : 
     response = {
@@ -10,27 +10,32 @@ def expand_query(sentence:str) -> dict :
         'language': '',
         'lemmatized_tokens': [],
         'stemmed_tokens': [],
-        'query_expansion': {},
-        'classification': '',
-        'entity': {},
-        'part_of_speech': {}
+        'query_expansion': [],
+        'classification': ''
+        #'entity': {},
+        #'part_of_speech': {}
     }
 
-    stemmed_tokens, lemmatized_tokens, corrected_sentence = normalize(sentence)
-    entity = get_entity(corrected_sentence)
-    part_of_speech = get_part_of_speech(corrected_sentence)
+    clean,stemmed_tokens, lemmatized_tokens, corrected_sentence = normalize(sentence)
+    #entity = get_entity(corrected_sentence)
+    classification = entity_recongnition(clean)
+    query_expan = query_expansion(corrected_sentence)
+    #part_of_speech = get_part_of_speech(corrected_sentence)
 
     
     response['corrected_sentence'] = corrected_sentence
     response['stemmed_tokens'] = stemmed_tokens
     response['lemmatized_tokens'] = lemmatized_tokens
-    response['entity'] = entity
-    response['part_of_speech'] = part_of_speech
-    
+    #response['entity'] = entity
+    #response['part_of_speech'] = part_of_speech
+    response['classification'] = classification
+    response['query_expansion'] = query_expan
 
     return response
 
 
 
 if __name__ == '__main__':
-    expand_query('!!#$%^tesiss  hablamos del 2019.')
+    final_query = expand_query('!!#$%^tesis del 2019.')
+    for value,key in final_query.items():
+        print(value,': ',key)
