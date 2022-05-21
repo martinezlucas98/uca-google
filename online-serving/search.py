51028534162adaec4538959d2ab6a103b645f0c9
l3on3l <fabianvera045@gmail.com>
2022-05-21 15:02:29 -0400

    backlinks with indexes

--- online-serving/search.py
+++ online-serving/search.py
@@ -44,8 +44,21 @@ def tokenize_query(query: str):
     #eliminar los stop words
     query_tokens = [tk for tk in query_tokens if tk not in STOP_WORDS]
     
-    #contamos la ocurrencia de cada token
     return query_tokens
+
+def calculate_backlinks(pages):
+    """
+        Calculamos los backlinks que tienen cada pagina.
+    """
+    for page in pages:
+        backlinks = []
+        for p in pages:
+            if p != page:
+                if page in pages[p]['links']:
+                    backlinks.append(p)
+        pages[page]['links'] = backlinks
+    return pages
+        
 
 def generate_pages_linked(indexes):
     """
@@ -58,23 +71,9 @@ def generate_pages_linked(indexes):
     linked_pages = {}
     for tk in indexes_keys:
         for item in indexes[tk]:
-            linked_pages[item['url']] = []
-
-    #enlazamos las paginas de manera aleatoria    
-    pages_keys = list(linked_pages.keys())
-    for page in linked_pages:
-        links = random.randint(1, len(linked_pages.keys()))
-        visited = []
-        visited.append(page)
-        while links > 0:
-            link = random.choice(pages_keys)
-            if (link not in visited):
-                    linked_pages[page].append(link)
-                    visited.append(link)
-            links -= 1
-        if not linked_pages[page]:
-            linked_pages[page].append(pages_keys[pages_keys.index(page)-1])   
-    return linked_pages      
+            linked_pages[item['url']] = {'title':item['title'], 'description':item['description'], 'links':item['links']}
+    linked_pages = calculate_backlinks(linked_pages)
+    return linked_pages
     
 def pagerank(indexes_pages, n=1000):
     """
@@ -101,7 +100,8 @@ def pagerank(indexes_pages, n=1000):
     #rankeamos las paginas
     ranked_pages = {}
     for page in indexes_pages.keys():
-        ranked_pages[page] = visited.count(page)/len(visited)
+        rank = visited.count(page)/len(visited)
+        ranked_pages[rank] = indexes_pages[page]
     return ranked_pages
 
 def get_next_page_prob(indexes_pages, current_page):
@@ -115,18 +115,19 @@ def get_next_page_prob(indexes_pages, current_page):
     # debemos saber que tan probable es visitar una pagina mediante links enlazados o ir directamente
     # a otra pagina (sin links).
     probability = {}
-    if len(indexes_pages[current_page]) == 0:
+    # print('get_next: ', indexes_pages[current_page]['links'])
+    if len(indexes_pages[current_page]['links']) == 0:
         #si no tenemos ningun otro link se visita cualquier otra pagina
         prob_page = 1/len(indexes_pages.keys())
     else:
         #prob. de cuando se visita directamente otra pagina (sin los links)
         prob_page = (1-DAMPING_FACTOR)/len(indexes_pages.keys())
         #prob. de seguir visitando paginas mediante los links
-        prob_link = DAMPING_FACTOR/len(indexes_pages[current_page])
+        prob_link = DAMPING_FACTOR/len(indexes_pages[current_page]['links'])
 
     #cargamos una distribucion de posibilidades de que pagina visitar de todas las posibles        
     for page in indexes_pages.keys():
-        if page in indexes_pages[current_page]:
+        if page in indexes_pages[current_page]['links']:
             probability[page] = prob_page + prob_link
         else:
             probability[page] = prob_page
@@ -142,18 +143,15 @@ def main(argv):
     tokens = tokenize_query(argv)
     #se obtiene la busqueda de cada token en los indices
     t1 = time.time()
-    indexes = get_index_request(tokens[0], "st")    
-    for i in range(1, len(tokens)):        
-        indexes.update(get_index_request(tokens[i], "st"))
+    indexes = get_index_request("+".join(tokens), "st")
 
     results = None            
     if (indexes):
         ranked_pages = pagerank(indexes)
+        # print('Ranked:\n', ranked_pages)
         ranked_pages = sorted(ranked_pages)
         t2 = time.time()
         ranked_pages = [{"url": page} for page in ranked_pages]
-        print()
-        print(ranked_pages)
         results = {
             "status": "success",
             "time": round(t2-t1, 3),
@@ -163,7 +161,8 @@ def main(argv):
         t2 = time.time()
         results = { "status": 'notfound', "time": round(t2-t1, 3), "results":argv }
     
+    print(results)
     return json.dumps(results)
         
 if __name__ == "__main__":   
-    main("¿Quien creo los testing?")
+    main("curso test?")
