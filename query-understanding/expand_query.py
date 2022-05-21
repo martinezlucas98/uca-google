ad03840f81d33740a57a3c1dd766c5096b5fcbaa
bamy16 <60363312+bamy16@users.noreply.github.com>
2022-05-20 20:20:19 -0400

    add language detection response

--- query-understanding/expand_query.py
+++ query-understanding/expand_query.py
@@ -1,6 +1,7 @@
 from text_normalization import  normalize
 from nlp_tools.named_entity import get_entity
 from nlp_tools.part_of_speech import get_part_of_speech
+from nlp_tools.lang_detect import language_detect
 from nlp_tools.query_expansion.query_expansion import query_expansion, entity_recongnition
 
 def expand_query(sentence:str) -> dict : 
@@ -17,25 +18,26 @@ def expand_query(sentence:str) -> dict :
     }
 
     clean,stemmed_tokens, lemmatized_tokens, corrected_sentence = normalize(sentence)
-    #entity = get_entity(corrected_sentence)
+    language = language_detect(clean)
     classification = entity_recongnition(clean)
     query_expan = query_expansion(corrected_sentence)
+    #entity = get_entity(corrected_sentence)
     #part_of_speech = get_part_of_speech(corrected_sentence)
 
     
     response['corrected_sentence'] = corrected_sentence
+    response['language'] = language
     response['stemmed_tokens'] = stemmed_tokens
     response['lemmatized_tokens'] = lemmatized_tokens
-    #response['entity'] = entity
-    #response['part_of_speech'] = part_of_speech
     response['classification'] = classification
     response['query_expansion'] = query_expan
-
+    #response['entity'] = entity
+    #response['part_of_speech'] = part_of_speech
     return response
 
 
 
 if __name__ == '__main__':
-    final_query = expand_query('!!#$%^tesis del 2019.')
+    final_query = expand_query('!!#$%^tesis del año 2019.')
     for value,key in final_query.items():
         print(value,': ',key)
