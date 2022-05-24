6723777d5c0ba7af1f96fd933275059a187c0f18
l3on3l <fabianvera045@gmail.com>
2022-05-23 20:23:16 -0400

    add online-service configuration

--- online-serving/settings.py
+++ online-serving/settings.py
@@ -8,7 +8,9 @@ HOST = "http://localhost"
 ## port
 PORT_INDEX = "8080" #port for index
 PORT_QUERY = "8081" #port for query understanding
+PORT_ONLINE = "8000" #port for my service
 
 #PATH (no config)
 PATH_INDEX = HOST + ":" + PORT_INDEX + "/"
 PATH_QUERY = HOST + ":" + PORT_QUERY + "/"
+PATH_ONLINE = HOST + ":" + PORT_ONLINE + "/"
