44a618b08d37206b0af3e7e2de9d535bf5d9d1e5
Ezequiel Pereira <ezequiell234242@gmail.com>
2022-06-18 16:12:38 -0400

    fix uc_data folder

--- offline_backend/crawler/pipelines.py
+++ offline_backend/crawler/pipelines.py
@@ -11,6 +11,7 @@ from itemadapter import ItemAdapter
 import hashlib
 import json
 import os
+import errno
 
 class CrawlerPipeline:
     def process_item(self, item, spider):
@@ -18,7 +19,13 @@ class CrawlerPipeline:
         url = item['url_self'][0].encode('utf-8')
 
         hash = hashlib.md5(url).hexdigest()
-       
+        
+        try:
+            os.mkdir(os.getcwd() + '/spiders/uc_data')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                pass
+    
         file = open(os.getcwd() + '/spiders/uc_data/uc_' + hash + '.json', "w+")
         json.dump(dict(item), file, indent = 4)
         file.close
